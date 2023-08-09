859cc5fd7596e17de427ba3bc1755c6591b84365
Simon Farshid <Yonom@users.noreply.github.com>
2023-08-09 18:24:57 +0200

    Init dashboard

---
+++ packages/firebase-example/tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./app/**/*.tsx", "./components/**/*.tsx"],
+  theme: {},
+  plugins: [],
+};
+
+export default config;
