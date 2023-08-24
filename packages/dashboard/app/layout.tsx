b77c36509f010bea46355d1868c0db0820b6762d
Simon Farshid <Yonom@users.noreply.github.com>
2023-08-24 08:07:56 +0200

    Add mintlify docs

--- packages/dashboard/app/layout.tsx
+++ packages/dashboard/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className="h-full dark" lang="en">
+      <body className={inter.className + " h-full"}>{children}</body>
     </html>
   );
 }
