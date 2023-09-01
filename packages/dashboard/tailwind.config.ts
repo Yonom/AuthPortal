c9846b8c45b2c5a0e8b17f0410762b4143cd83d7
Simon Farshid <Yonom@users.noreply.github.com>
2023-09-01 09:20:03 +0200

    a

--- packages/dashboard/tailwind.config.ts
+++ packages/dashboard/tailwind.config.ts
@@ -1,9 +1,15 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: ["./app/**/*.tsx", "./components/**/*.tsx"],
-  theme: {},
-  plugins: [],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
 };
 
 export default config;
